2c4e7a466c7bc4aacf10c97f70845e12db1a27eb
iiLandr <46393231+iiLandr@users.noreply.github.com>
2019-01-06 05:39:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,43 +13,7 @@ client.on('message', msg => {
 });
 
 
-const developers = ["323152294289866763"] // Toxic Codes
-client.on ('message', message => {// Toxic Codes
-    var argresult = message.content.split (``) .slice (1) .join ('')؛ // Toxic Codes
-      if (! developers.includes (message.author.id)) return؛
-      
-  if (message.content.startsWith (adminprefix + 'setg')) {
-    client.user.setGame (argresult)؛
-      message.channel.send (`** ✅ $ {argresult} **`)
-  آخر 
-     if (message.content === (adminprefix + "leave")) {// Toxic Codes
-    message.guild.leave ()؛ // Toxic Codes
-  آخر  
-  if (message.content.startsWith (adminprefix + 'setw')) {
-  client.user.setActivity (argresult ، {type: 'WATCHING'})؛ // Toxic Codes
-      message.channel.send (`** ✅ $ {argresult} **`) // Toxic Codes
-  آخر 
-  if (message.content.startsWith (adminprefix + 'setl')) {
-  client.user.setActivity (argresult ، {type: 'LISTENING'})؛
-      message.channel.send (`** ✅ $ {argresult} **`) // Toxic Codes
-  آخر 
-  if (message.content.startsWith (adminprefix + 'sets')) {
-    client.user.setGame (argresult، "https://www.twitch.tv/zero")؛
-      message.channel.send (`** ✅ **`) // Toxic Codes
-  }
-  if (message.content.startsWith (adminprefix + 'setname')) {
-  client.user.setUsername (argresult). ثم
-      message.channel.send (`تغيير الاسم إلى .. ** $ {argresult} **`)
-آخر
-  if (message.content.startsWith (adminprefix + 'setprefix')) {// Toxic Codes
-  client.user.setPrefix (argresult). ثم
-      message.channel.send (`Preging Changing .. ** $ {argresult} **`) // Toxic Codes
-آخر
-if (message.content.startsWith (adminprefix + 'setavatar')) {// Toxic Codes
-  client.user.setAvatar (argresult)؛
-    message.channel.send (`تغيير الصورة الرمزية إلى: ** $ {argresult} **`)؛ // Toxic Codes
-}
-}) ؛ // الرموز السامة
+
 
 
 
