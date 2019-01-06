5f4350298302fcb8b6edce5976ad623d9a0e05c7
iiLandr <46393231+iiLandr@users.noreply.github.com>
2019-01-06 07:59:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ client.on('message', message => {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**✅   ${argresult}**`)
   } else 
-  if (message.content.startsWith('(prefix)st')) {
+  if (message.content.startsWith('(+)st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/idk");
       message.channel.send(`**✅**`)
   }
