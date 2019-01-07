2a2f3f55e479ce9be64a57542ca81932a8c5efbc
iiLandr <46393231+iiLandr@users.noreply.github.com>
2019-01-07 07:26:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,39 +16,40 @@ client.on('message', msg => {
 
 
 const devs = ["323152294289866763"];
-const adminprefix = ["+"];
+const adminprefix = "+";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
       
-  if (message.content.startsWith('(+))ply')) {
+  if (message.content.startsWith(adminprefix + "ply")) {
     client.user.setGame(argresult);
       message.channel.send(`**✅   ${argresult}**`)
   } else 
-     if (message.content === ("leave")) {
+     if (message.content === ( adminprefix + "leave")) {
     message.guild.leave();        
   } else  
   if (message.content.startsWith('(prefix)wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.send(`**✅   ${argresult}**`)
   } else 
-  if (message.content.startsWith('mils')) {
+  if (message.content.startsWith(adminprefix + "wt")) {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**✅   ${argresult}**`)
   } else 
-  if (message.content.startsWith('(prefix)st')) {
+  if (message.content.startsWith(adminprefix +"st")) {
     client.user.setGame(argresult, "https://www.twitch.tv/idk");
       message.channel.send(`**✅**`)
   }
-  if (message.content.startsWith('(prefix)setname')) {
+  if (message.content.startsWith(adminprefix + "setname")) {
   client.user.setUsername(argresult).then
       message.channel.send(`Changing The Name To ..**${argresult}** `)
 } else
-if (message.content.startsWith('(prefix)setavatar')) {
+if (message.content.startsWith(adminprefix + "setavatar")) {
   client.user.setAvatar(argresult);
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
 });
+
 
 
 
